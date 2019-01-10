7f4d65139fb30aa61ab883dd1a86a78ca6a322bf
chenlan <chenlan@chenlandeMacBook-Pro.local>
2019-01-10 09:24:24 +0800

    11

--- client/index.js
+++ client/index.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import App from './app.vue'
 
-import './assets/styles/global.styl'
+Vue.config.productionTip = false
 
-const root = document.createElement('div')
-document.body.appendChild(root)
-
+/* eslint-disable no-new */
 new Vue({
-  render: (h) => h(App)
-}).$mount(root)
+  el: '#app',
+  template: '<App/>',
+  components: { App }
+})
